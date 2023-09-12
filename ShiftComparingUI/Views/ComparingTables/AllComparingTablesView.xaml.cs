165ab50e855fdf167e2e9395b30a052d58c00a46
Sebastian H <sebastianheyde@t-online.de>
2023-09-12 07:48:26 +0200

    added personoverview (not finished)

---
+++ ShiftComparingUI/Views/ComparingTables/AllComparingTablesView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftComparingUI.Views.ComparingTables;
+
+public partial class AllComparingTablesView : ContentPage
+{
+    public AllComparingTablesView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ShiftComparingUI/Views/Shiftsystems/AllShiftsystemsView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftComparingUI.Views.Shiftsystems;
+
+public partial class AllShiftsystemsView : ContentPage
+{
+    public AllShiftsystemsView()
+    {
+        InitializeComponent();
+    }
+}
